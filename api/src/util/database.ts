bf5b557667bc59c21067361558ecf92a2b55f14b
Claudio Matus Alegría <claudio@matus.cl>
2022-09-22 03:04:36 -0300

    Feature/Database Cluster

--- api/src/util/database.ts
+++ api/src/util/database.ts
@@ -1,9 +1,21 @@
 import pg from "pg";
+import path from "path";
+import fs from "fs";
 
 import config from "./config";
+import createLogger from "./logger";
 
 const { Pool } = pg;
 const { dbHost, dbUser, dbPassword, dbName } = config;
+
+/* 
+  username = linpostgres
+  password = VTsTvtUE2l^92si9
+  host = lin-9038-1973-pgsql-primary.servers.linodedb.net
+  private network host = lin-9038-1973-pgsql-primary-private.servers.linodedb.net
+  port = 5432
+  ssl = ENABLED
+*/
 
 const pool = new Pool({
   user: dbUser,
@@ -11,6 +23,24 @@ const pool = new Pool({
   database: dbName,
   password: dbPassword,
   port: 5432,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "/certificate.crt")),
+  },
+});
+
+pool.connect(function (err) {
+  if (err) {
+    createLogger.error({
+      util: "database",
+      error: err,
+    });
+    return;
+  }
+
+  createLogger.info({
+    util: "database",
+    message: "Database connected",
+  });
 });
 
 export default pool;
