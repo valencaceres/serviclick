9add6df0636ca4f8d772cd8a8444c4e0cd8cce82
Claudio Matus Alegría <claudio@matus.cl>
2023-02-28 18:00:16 -0300

    feature/alliances

--- web-alliance/src/components/functional/CardProduct/CardProduct.tsx
+++ web-alliance/src/components/functional/CardProduct/CardProduct.tsx
@@ -11,28 +11,34 @@ import { currencyFormat } from "../../../utils/format";
 const CardProduct = ({ product, onClick }: any) => {
   return (
     <Card>
-      <Image
-        src={`/images/products/thumbs/${product.product_id}.jpg`}
-        alt={product.name}
-        width={300}
-        height={247}
-      />
-      <h2>{product.name}</h2>
-      <div className={style.info}>Podrás obtener hasta copago $0</div>
-      <div className={style.promotional}>{product.promotional}</div>
-      <div className={style.price}>
-        Contrátalo por solo {currencyFormat(product.price.company)}
+      <div className={style.cardContain}>
+        <div className={style.left}>
+          <Image
+            src={`/images/products/thumbs/${product.product_id}.jpg`}
+            alt={product.name}
+            width={300}
+            height={247}
+          />
+        </div>
+        <div className={style.right}>
+          <h2>{product.name}</h2>
+          <div className={style.info}>Podrás obtener hasta copago $0</div>
+          <div className={style.promotional}>{product.promotional}</div>
+          <div className={style.price}>
+            Contrátalo por solo {currencyFormat(product.price.company)}
+          </div>
+          <div className={style.priceNormal}>
+            Valor Normal <span>{currencyFormat(product.price.normal)}</span>
+          </div>
+          <Button
+            width="135px"
+            type="web"
+            text="¡Lo quiero!"
+            onClick={() => onClick(product.productplan_id)}
+          />
+          <a className={style.link}>Descargar PDF</a>
+        </div>
       </div>
-      <div className={style.priceNormal}>
-        Valor Normal <span>{currencyFormat(product.price.normal)}</span>
-      </div>
-      <Button
-        width="135px"
-        type="web"
-        text="¡Lo quiero!"
-        onClick={() => onClick(product.productplan_id)}
-      />
-      <a className={style.link}>Descargar PDF</a>
     </Card>
   );
 };
