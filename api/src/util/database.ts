55037d127e06cfdde722545e4350c69371dba2e0
feldrok <matias@moravalero.cl>
2023-03-31 19:06:29 -0300

    rollback to prod

--- api/src/util/database.ts
+++ api/src/util/database.ts
@@ -24,6 +24,9 @@ const pool = new Pool({
   password: dbPassword,
   port: 5432,
   keepAlive: true,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "/certificate.crt")),
+  },
 });
 
 pool.connect(function (err) {
