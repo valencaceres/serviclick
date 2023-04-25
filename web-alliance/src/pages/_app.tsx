5227a368b78adc929e33a9a13dedeb2db0f4659e
Claudio Matus Alegría <claudio@matus.cl>
2023-04-24 21:36:36 -0400

    feature/google-analytics-los-heroes

--- web-alliance/src/pages/_app.tsx
+++ web-alliance/src/pages/_app.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/inline-script-id */
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
@@ -12,6 +14,20 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-FBMKNKD7PQ`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-FBMKNKD7PQ', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Head>
         <title>
           ServiClick.cl - Todas las soluciones para tu hogar, en la palma de tu
