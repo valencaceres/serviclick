6858c80e8cdd2932e5701160993417571f8d485c
Matias Mora <matias@moravalero.cl>
2023-08-16 11:13:13 -0400

    feat: add security headers and hide poweredBy next

--- operations/src/utils/api.ts
+++ operations/src/utils/api.ts
@@ -4,7 +4,13 @@ import { config } from "../utils/config";
 
 const apiInstance = axios.create({
   baseURL: `${config.server}/api`,
-  headers: { id: config.apiKey },
+  headers: {
+    id: config.apiKey,
+    "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
+    "X-Frame-Options": "DENY",
+    "X-Content-Type-Options": "nosniff",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+  },
 });
 
 export { apiInstance };
