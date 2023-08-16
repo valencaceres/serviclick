2289a83abd07aff105ea3fd8298cd5a0174ef1da
Matias Mora <matias@moravalero.cl>
2023-08-16 11:00:51 -0400

    feat: cors for production

---
+++ api/src/util/allowedOrigins.ts
@@ -0,0 +1,10 @@
+export const allowedOrigins = [
+  "https://operaciones.serviclick.cl/",
+  "https://app.serviclick.cl",
+  "https://serviclick.cl",
+  "https://www.serviclick.cl",
+  "https://broker.serviclick.cl",
+  "https://productos.serviclick.cl",
+  "https://admin.serviclick.cl",
+  "https://webhook.serviclick.cl",
+];
