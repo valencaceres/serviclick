980e37eed85448aedecb034268d804db43960cd3
Matias Mora <matias@moravalero.cl>
2023-08-21 12:57:17 -0400

    feat: add security headers

--- serviclick/src/utils/api.ts
+++ serviclick/src/utils/api.ts
@@ -4,7 +4,13 @@ import { config } from "../utils/config";
 
 const apiInstance = axios.create({
   baseURL: `${config.server}/api`,
-  headers: { id: config.apiKey },
+  headers: {
+    id: config.apiKey,
+    "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
+    "X-Frame-Options": "DENY",
+    "X-Content-Type-Options": "nosniff",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+  },
 });
 
 apiInstance.interceptors.request.use(
@@ -25,6 +31,10 @@ const requestHeaders = () => {
   return {
     id: config.apiKey,
     Authorization: sessionCookie ? `Bearer ${sessionCookie}` : undefined,
+    "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
+    "X-Frame-Options": "DENY",
+    "X-Content-Type-Options": "nosniff",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
   };
 };
 
