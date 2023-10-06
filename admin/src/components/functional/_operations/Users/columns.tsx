ed1f7d081e544e0909e505c406f2df4b45964c09
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-10-06 13:00:33 -0300

    fix dashboard

--- admin/src/components/functional/_operations/Users/columns.tsx
+++ admin/src/components/functional/_operations/Users/columns.tsx
@@ -1,40 +1,7 @@
 "use client";
 
 import { type Row, type ColumnDef } from "@tanstack/react-table";
-import { Badge } from "~/components/ui/Badge";
-import { api, type RouterOutputs } from "~/utils/api";
-import { cn } from "~/utils/cn";
-import { Loader2, MoreHorizontal, Paperclip } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "~/components/ui/DropdownMenu";
-import { Button } from "~/components/ui/Button";
-import { useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/Dialog";
-import { Label } from "~/components/ui/Label";
-import { Input } from "~/components/ui/Input";
-import { Textarea } from "~/components/ui/Textarea";
-import { useUser } from "@clerk/nextjs";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "~/components/ui/Accordion";
-import Link from "next/link";
+
 import { type User } from "@clerk/nextjs/dist/types/server";
 
 export const columns: ColumnDef<User>[] = [
