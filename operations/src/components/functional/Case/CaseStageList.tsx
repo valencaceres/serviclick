486f799e9d33bab61673ad5abdd1d778d3879817
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-10-30 06:27:27 -0300

    case Stage list and case chat

--- operations/src/components/functional/Case/CaseStageList.tsx
+++ operations/src/components/functional/Case/CaseStageList.tsx
@@ -21,24 +21,17 @@ import { useQueryLead } from "~/hooks/query";
 import { useCase } from "~/store/hooks/useCase";
 import Button from "../../ui/Button";
 import Link from "next/link";
+import { format } from "date-fns";
 
-const CaseStageList = ({ showModal, setShowModal }: any) => {
+const CaseStageList = ({ showModal, setShowModal, caseValue }: any) => {
   const router = useRouter();
-  const id = router.query.id;
-
-  const { getById, caseData } = useCase();
-  useEffect(() => {
-    if (typeof id === "string") {
-      getById(id);
-    }
-  }, [id]);
 
   const {
     data: contract,
     isError,
     isLoading,
     isFetching,
-  } = useQueryLead().useGetContract(caseData?.lead_id ?? "");
+  } = useQueryLead().useGetContract(caseValue?.lead_id ?? "");
 
   return (
     <Fragment>
@@ -55,56 +48,50 @@ const CaseStageList = ({ showModal, setShowModal }: any) => {
             <TableCellEnd />
           </TableHeader>
           <TableDetail>
-            {caseData?.history
-              ?.filter(
-                (stage: any) =>
-                  stage.stage !== "Solicitud reembolso" &&
-                  stage.stage !== "Descuento IMED"
-              )
-              .map((stage: any, idx: number) => (
-                <TableRow key={idx}>
-                  <TableCell width="95px" align="center">
-                    {new Date(stage.createddate).toISOString().substring(0, 10)}
-                  </TableCell>
-                  <TableCell width="57px">
-                    {new Date(stage.createddate)
-                      .toISOString()
-                      .substring(11, 16)}
-                  </TableCell>
-                  <TableCell width="177px" align="center">
-                    {stage.user?.first_name + " " + stage.user?.last_name}
-                  </TableCell>
-                  <TableCell width="208px" align="center">
-                    {stage.stage}
-                  </TableCell>
-                  <TableCell width="41px" align="center">
-                    <TableIcons>
-                      <Icon
-                        iconName="search"
-                        button={true}
-                        onClick={() => {
-                          router.push(
-                            `/case/${
-                              caseData?.case_id
-                            }/${stage.stage.toLowerCase()}`
-                          );
-                        }}
-                      />
-                    </TableIcons>
-                  </TableCell>
-                </TableRow>
-              ))}
+            {caseValue?.history?.map((stage: any, idx: number) => (
+              <TableRow key={idx}>
+                <TableCell width="95px" align="center">
+                  {stage?.date}
+                </TableCell>
+                <TableCell width="57px">{stage?.time}</TableCell>
+                <TableCell width="177px" align="center">
+                  {stage.user?.first_name + " " + stage.user?.last_name}
+                </TableCell>
+                <TableCell width="208px" align="center">
+                  {stage.name}
+                </TableCell>
+                <TableCell width="41px" align="center">
+                  <TableIcons>
+                    <Icon
+                      iconName="search"
+                      button={true}
+                      onClick={() => {
+                        router.push(
+                          `/assistance/case/${stage.code.toLowerCase()}/${
+                            caseValue?.case_id
+                          }`
+                        );
+                      }}
+                    />
+                  </TableIcons>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableDetail>
         </Table>
         <ContentRow className="justify-between">
           <ContentCellSummary
-            color={caseData?.history?.length > 0 ? "blue" : "#959595"}
+            color={
+              caseValue && caseValue.history && caseValue.history.length > 0
+                ? "blue"
+                : "#959595"
+            }
           >
-            {caseData?.history?.length === 0
+            {caseValue?.history?.length === 0
               ? "No hay acciones"
-              : caseData?.history?.length === 1
+              : caseValue?.history?.length === 1
               ? "1 acción"
-              : `${caseData?.history?.length} acciones`}
+              : `${caseValue?.history?.length} acciones`}
           </ContentCellSummary>
           <div className="flex gap-2">
             {contract && !isError && !isLoading && !isFetching && (
--- operations/src/pages/assistance/case/refund/[id].tsx
+++ operations/src/pages/assistance/case/refund/[id].tsx
@@ -6,18 +6,38 @@ import { useUI } from "~/hooks";
 
 import CaseFormRefund from "~/components/functional/Case/CaseFormRefund";
 import { ContentCell } from "~/components/layout/Content";
+import CaseNotes from "~/components/functional/Case/CaseChat";
+import { Modal, Window } from "~/components/ui/Modal";
+import { useCase } from "~/store/hooks/useCase";
 const Applicant = () => {
   const router = useRouter();
   const [showModal, setShowModal] = useState(false);
+  const setClosed = () => setShowModal(false);
+  const id = router.query.id;
 
+  const { getById, caseValue } = useCase();
+  useEffect(() => {
+    if (typeof id === "string") {
+      getById(id);
+    }
+  }, [id]);
   return (
     <Fragment>
       <ContentHalfRow>
         <CaseFormRefund />
         <ContentCell gap="20px">
-          <CaseStageList setShowModal={setShowModal} showModal={showModal} />
+          <CaseStageList
+            caseValue={caseValue}
+            setShowModal={setShowModal}
+            showModal={showModal}
+          />
         </ContentCell>
       </ContentHalfRow>
+      <Modal showModal={showModal}>
+        <Window setClosed={setClosed}>
+          <CaseNotes thisCase={caseValue} />
+        </Window>
+      </Modal>
     </Fragment>
   );
 };
