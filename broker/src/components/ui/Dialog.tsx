46b605d50ed0cd43f0c3d1de9f99d14021a659c9
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-11-06 14:48:06 -0300

    classname fix

--- broker/src/components/ui/Dialog.tsx
+++ broker/src/components/ui/Dialog.tsx
@@ -4,18 +4,17 @@ import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 
-import { cn } from "~/utils/cn"; 
+import { cn } from "~/utils/cn";
 
 const Dialog = DialogPrimitive.Root;
 
 const DialogTrigger = DialogPrimitive.Trigger;
 
 const DialogPortal = ({
-  className,
   children,
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props}>
+  <DialogPrimitive.Portal {...props}>
     <div className="fixed inset-0 z-50 flex items-start justify-center sm:items-center">
       {children}
     </div>
