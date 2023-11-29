f4b8c5c887feda1d4a570ffbad993ad8b1925f4b
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-11-29 14:18:30 -0300

    cn

--- www/components/functional/home/hero-carousel.tsx
+++ www/components/functional/home/hero-carousel.tsx
@@ -2,6 +2,8 @@
 
 import { EffectFade, Navigation, Pagination, SwiperOptions } from "swiper"
 
+import { cn } from "@/lib/utils"
+
 import "swiper/css"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
@@ -50,26 +52,27 @@ export const HeroCarousel = ({
 
   const slides = heroData.map((heroItem) => (
     <section className="relative flex h-[250px] w-full cursor-pointer items-center justify-center md:h-[550px] md:justify-start">
+      <Image
+        src={heroItem.url}
+        alt={heroItem.alt}
+        fill
+        quality={100}
+        className="cursor-pointer object-cover object-top"
+        loading="lazy"
+      />
+      <h1 className="absolute z-40 w-full max-w-xs text-center text-5xl uppercase text-white md:max-w-md md:pl-20 md:text-start md:text-6xl">
+        {heroItem.text}
+      </h1>
+
       <Link
         key={heroItem.id}
         href={`${heroItem.link}`}
         passHref
         target="_blank"
-        className="z-50 border border-blue-500"
-      >
-        <Image
-          src={heroItem.url}
-          alt={heroItem.alt}
-          fill
-          quality={100}
-          className="cursor-pointer object-cover object-top"
-          loading="lazy"
-        />
-        <h1 className="absolute z-40 w-full max-w-xs text-center text-5xl uppercase text-white md:max-w-md md:pl-20 md:text-start md:text-6xl">
-          {heroItem.text}
-        </h1>
-      </Link>
-      <div className="absolute left-0 top-0 z-10 h-full w-full bg-black bg-opacity-30"></div>
+        className={cn("absolute left-0 top-0 z-30 h-full w-full", {
+          "bg-black bg-opacity-30": heroItem.text,
+        })}
+      ></Link>
     </section>
   ))
 
