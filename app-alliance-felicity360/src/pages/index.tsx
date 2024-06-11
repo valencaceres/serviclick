c7d09f898703796b631db54db8778f9f0c307ff9
Amelia <amelia.feliu06@gmail.com>
2024-06-11 10:00:04 -0400

    commit de prueba

--- app-alliance-felicity360/src/pages/index.tsx
+++ app-alliance-felicity360/src/pages/index.tsx
@@ -1,3 +1,20 @@
+import Head from "next/head";
+
+import Hero from "@/components/ui/Hero/Hero";
+
 export default function Home() {
-  return <div>Hola</div>;
+  return (
+    <>
+      <Head>
+        <title>Serviclick - Todas las soluciones en la palma de tu mano</title>
+        <meta name="description" content="Serviclick - Todas las soluciones en la palma de tu mano" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/img/hero/logocolor.png" />
+      </Head>
+
+      <main>
+        <Hero />
+      </main>
+    </>
+  )
 }
