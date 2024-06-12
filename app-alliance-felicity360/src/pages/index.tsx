798ceaf90a56b3fbf62b958bed0c5b5c112aef84
Amelia <amelia.feliu06@gmail.com>
2024-06-12 10:27:51 -0400

    Paragraph

--- app-alliance-felicity360/src/pages/index.tsx
+++ app-alliance-felicity360/src/pages/index.tsx
@@ -1,19 +1,36 @@
 import Head from "next/head";
 
 import Hero from "@/components/ui/Hero/Hero";
+import Paragraph from "@/components/ui/Paragraph/Paragraph";
 
 export default function Home() {
+  const content = "Junto a Felicity 360, te brindamos las mejores Asistencias diseñadas especialmente para ti y tu seres queridos. ";
+
+
+  const wordsWithStyles = [
+    {
+
+
+      word: "360,", color: "#29ABE2", fontWeight: "bolder"
+    },
+    {
+      word: "Felicity", color: "#DE0079", fontWeight: "bolder"
+    },
+
+  ];
+
   return (
     <>
       <Head>
         <title>Serviclick - Todas las soluciones en la palma de tu mano</title>
         <meta name="description" content="Serviclick - Todas las soluciones en la palma de tu mano" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/img/hero/logocolor.png" />
+        <link rel="icon" href="/img/hero/logocolor.png" />
       </Head>
 
       <main>
-        <Hero title="ASISTENCIAS QUE TE PROTEGEN <br> EN TODO MOMENTO" firstLogo="/img/hero/serviclick.png" secondLogo="/img/hero/logo-felicity-white.png"/>
+        <Hero title="ASISTENCIAS QUE TE PROTEGEN <br> EN TODO MOMENTO" firstLogo="/img/hero/serviclick.png" secondLogo="/img/hero/logo-felicity-white.png" />
+        <Paragraph content={content} wordsWithStyles={wordsWithStyles} />
       </main>
     </>
   )
