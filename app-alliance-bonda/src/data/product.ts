deb0b75e604b7bcf781e8eb2c66ab152cbb67e35
Amelia <amelia.feliu06@gmail.com>
2024-06-27 14:31:46 -0400

    sin mensaje

--- app-alliance-bonda/src/data/product.ts
+++ app-alliance-bonda/src/data/product.ts
@@ -1,36 +1,36 @@
-export const productData = [
-  {
-    title: "Asistencia Integral Pro ",
-    paragraph:
-      "Podrás obtener beneficios en: Urgencia Médica por Accidente, Urgencia Médica  por Enfermedad, Atención Médica Ambulatoria,  Descuento en Farmacias, Telemedicina, Orientación Médica Telefónica,  Médico a Domicilio. ",
-    traced: " $14.990",
-    priceText: "$11.990",
-    discountText: "20%",
-    beneficiaryText: "$3.590 (cada carga)",
-    buttonLink: "/detail?prod=integralPro",
-    buttonText: "VER MÁS",
-    img: "/img/card/asistencia1.png",
-  },
-  {
-    title: "Asistencia Mascota Pro",
-    paragraph:
-      "Podrás obtener beneficios en: Urgencia Veterinaria, Consulta Médica, Descuento en Farmacias, Telemedicina, Asistencia Legal Telefónica, Vacuna Antirrábica.",
-    traced: " $8.590",
-    priceText: "$6.870",
-    discountText: "20%",
-    buttonLink: "/detail?prod=mascotalPro",
-    buttonText: "VER MÁS",
-    img: "/img/card/asistencia2.png",
-  },
-  {
-    title: "Asistencia Hogar Pro ",
-    paragraph:
-      "Podrás obtener beneficios en: Plomería, Cerrajería, Servicios de Electricidad, Instalación de Lámparas e Iluminaria, Servicios de Pintura Baño y Cocina, Perforaciones en Muro. ",
+ export const productData = [
+   {
+     title: "Asistencia Integral Pro ",
+     paragraph:
+       "Podrás obtener beneficios en: Urgencia Médica por Accidente, Urgencia Médica  por Enfermedad, Atención Médica Ambulatoria,  Descuento en Farmacias, Telemedicina, Orientación Médica Telefónica,  Médico a Domicilio. ",
+     traced: " $14.990",
+     priceText: "$11.990",
+     discountText: "20%",
+     beneficiaryText: "$3.590 (cada carga)",
+     buttonLink: "/detail?prod=integralPro",
+     buttonText: "VER MÁS",
+     img: "/img/card/asistencia1.png",
+   },
+   {
+     title: "Asistencia Mascota Pro",
+     paragraph:
+       "Podrás obtener beneficios en: Urgencia Veterinaria, Consulta Médica, Descuento en Farmacias, Telemedicina, Asistencia Legal Telefónica, Vacuna Antirrábica.",
+     traced: " $8.590",
+     priceText: "$6.870",
+     discountText: "20%",
+     buttonLink: "/detail?prod=mascotalPro",
+     buttonText: "VER MÁS",
+     img: "/img/card/asistencia2.png",
+   },
+   {
+     title: "Asistencia Hogar Pro ",
+     paragraph:
+       "Podrás obtener beneficios en: Plomería, Cerrajería, Servicios de Electricidad, Instalación de Lámparas e Iluminaria, Servicios de Pintura Baño y Cocina, Perforaciones en Muro. ",
     traced: " $9.490",
     priceText: "$7.590",
     discountText: "20%",
     buttonLink: "/detail?prod=hogarPro",
-    buttonText: "VER MÁS",
-    img: "/img/card/asistencia3.png",
-  },
+     buttonText: "VER MÁS",
+     img: "/img/card/asistencia3.png",
+   },
 ];
