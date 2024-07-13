397eb1bcb02a81dafeb5f5b4afafd30ee86d0a08
Claudio Matus Alegría <claudio@matus.cl>
2024-07-13 17:42:05 -0400

    fix/title

--- web-alliance-bonda/src/pages/detail/[id].tsx
+++ web-alliance-bonda/src/pages/detail/[id].tsx
@@ -1,16 +1,16 @@
 import Head from "next/head";
 
 import Detail from "@/components/functional/Detail";
+
 import { useProduct } from "@/store/hooks";
 
 const DetailPage = () => {
-
-  const {product} = useProduct()
+  const { product } = useProduct();
 
   return (
     <>
       <Head>
-        <title>{product.name}</title>
+        <title>{product.product_name}</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/img/hero/icono.png" />
--- web-alliance-felicity360/src/pages/detail/[id].tsx
+++ web-alliance-felicity360/src/pages/detail/[id].tsx
@@ -2,11 +2,15 @@ import Head from "next/head";
 
 import Detail from "@/components/functional/Detail";
 
+import { useProduct } from "@/store/hooks";
+
 const DetailPage = () => {
+  const { product } = useProduct();
+
   return (
     <>
       <Head>
-        <title>Asistencia Integral Pro</title>
+        <title>{product.product_name}</title>
         <meta name="description" content="Asistencia Integral Pro" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/img/hero/icono.png" />
