692e14ec038ceb6d506f159f45dc56406d84f78b
Amelia <amelia.feliu06@gmail.com>
2024-08-01 14:23:43 -0400

    Avance 01/08  bancochile 2

--- web-alliance-bancochile/src/utils/format.ts
+++
@@ -1,9 +0,0 @@
-const formatPrice = (value: string) => {
-  const numericValue = parseFloat(value);
-  if (isNaN(numericValue)) {
-    return "";
-  }
-  return numericValue.toLocaleString("es-CL");
-};
-
-export { formatPrice };
