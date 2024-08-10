15dabf84bd58701daa5eff97c65cc1f84fc54aa1
Claudio Matus Alegría <claudio@matus.cl>
2024-08-10 10:50:53 -0400

    fix/prepago-los-heroes

--- web-alliance-prepagolosheroes/src/components/functional/CardProduct/CardProduct.tsx
+++ web-alliance-prepagolosheroes/src/components/functional/CardProduct/CardProduct.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { useRouter } from "next/router";
 
 import Card from "../../ui/Card";
 import Button from "../../ui/Button";
@@ -8,13 +7,33 @@ import style from "./CardProduct.module.scss";
 
 import { currencyFormat } from "../../../utils/format";
 
+import freeMonth from "./images/free-month.png";
+
+import img0e0566e816f6489888cccf98c6b37bed from "./images/thumbs/0f1827b5-a1f6-4b75-8392-171f83870ebe.jpg";
+import img3239e6c902174bd595ad7a0bc3fe07e6 from "./images/thumbs/3239e6c9-0217-4bd5-95ad-7a0bc3fe07e6.jpg";
+import img8370162d68fb417b8630180331832d26 from "./images/thumbs/8370162d-68fb-417b-8630-180331832d26.jpg";
+import img89f393326f154eeab6cd330aad768d7b from "./images/thumbs/89f39332-6f15-4eea-b6cd-330aad768d7b.jpg";
+import img8ec49ad42d6447cf949e18e05155221e from "./images/thumbs/8ec49ad4-2d64-47cf-949e-18e05155221e.jpg";
+import imgca51624eb2d84b30a2d48eade94d47c3 from "./images/thumbs/ca51624e-b2d8-4b30-a2d4-8eade94d47c3.jpg";
+
+const jsonImage: { [key: string]: any } = {
+  "0f1827b5-a1f6-4b75-8392-171f83870ebe": img0e0566e816f6489888cccf98c6b37bed,
+  "3239e6c9-0217-4bd5-95ad-7a0bc3fe07e6": img3239e6c902174bd595ad7a0bc3fe07e6,
+  "8370162d-68fb-417b-8630-180331832d26": img8370162d68fb417b8630180331832d26,
+  "89f39332-6f15-4eea-b6cd-330aad768d7b": img89f393326f154eeab6cd330aad768d7b,
+  "8ec49ad4-2d64-47cf-949e-18e05155221e": img8ec49ad42d6447cf949e18e05155221e,
+  "ca51624e-b2d8-4b30-a2d4-8eade94d47c3": imgca51624eb2d84b30a2d48eade94d47c3,
+};
+
 const CardProduct = ({ product, onClick }: any) => {
+  const img = jsonImage[product.id];
+
   return (
     <Card>
       <div className={style.cardContain}>
         <div className={style.left}>
           <Image
-            src={`/images/products/thumbs/${product.product_id}.jpg`}
+            src={img.src}
             alt={`producto-${product.id}`}
             width={300}
             height={247}
@@ -43,7 +62,9 @@ const CardProduct = ({ product, onClick }: any) => {
             rel="noreferrer">
             Descargar PDF
           </a>
-          <div className={style.freeMonth}></div>
+          <div
+            className={style.freeMonth}
+            style={{ backgroundImage: `url(${freeMonth.src})` }}></div>
         </div>
       </div>
     </Card>
