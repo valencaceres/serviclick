135a022bac906784d16de5c9b71dcb70748ea2b2
Claudio Matus Alegría <claudio@matus.cl>
2024-08-10 01:13:12 -0400

    fix/prepago-los-heroes

--- web-alliance-prepagolosheroes/next.config.js
+++ web-alliance-prepagolosheroes/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: `/${process.env.NEXT_PUBLIC_ASSET_PREFIX}` || "",
 };
 
 module.exports = nextConfig;
