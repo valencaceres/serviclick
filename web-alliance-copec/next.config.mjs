cf67d836edc2141722203794dcf6cc568e15932e
Claudio Matus Alegría <claudio@matus.cl>
2024-08-11 23:10:54 -0400

    feature-copec

--- web-alliance-copec/next.config.mjs
+++ web-alliance-copec/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/copec",
+  assetPrefix: `/${process.env.NEXT_PUBLIC_ASSET_PREFIX}` || "",
 };
 
 export default nextConfig;
