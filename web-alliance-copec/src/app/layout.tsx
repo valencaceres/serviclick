2146dfadc8cdfdf240fdda5eedfecbbc3ed3a205
Claudio Matus Alegría <claudio@matus.cl>
2024-08-11 21:10:38 -0400

    feature-copec

--- web-alliance-copec/src/app/layout.tsx
+++ web-alliance-copec/src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icono.png" />
+      </head>
       <body className={inter.className + " text-font-color"}>
         <Header />
         {children}
