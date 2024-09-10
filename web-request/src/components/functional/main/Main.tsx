400484370326aebcd79f01865dd819f6ad0709dc
Claudio Matus Alegría <claudio@matus.cl>
2024-09-09 22:35:57 -0300

    fix

--- web-request/src/components/functional/main/Main.tsx
+++ web-request/src/components/functional/main/Main.tsx
@@ -1,104 +1,105 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-import Button from '@/components/ui/Button'
-import Input from '@/components/ui/Input'
-import { ContentCol, ContentRow } from '@/components/layout/General/General'
-import { Table, TableCell, TableHeader, TableRow, TableDetail, TableCellEnd } from '@/components/ui/Table'
+import Button from "@/components/ui/Button";
+import Input from "@/components/ui/InputText";
+import { ContentCol, ContentRow } from "@/components/layout/General/General";
+import {
+  Table,
+  TableCell,
+  TableHeader,
+  TableRow,
+  TableDetail,
+  TableCellEnd,
+} from "@/components/ui/Table";
 
-import { useContractor } from '@/store/hooks'
-import format from '@/utils/format'
+import { useContractor } from "@/store/hooks";
+import format from "@/utils/format";
 
 const Main = () => {
-  const [rut, setRut] = useState('')
-  const [isActive, setIsActive] = useState(false)
-  const [openTable, setOpenTable] = useState(0)
-  const {getByRutOrName, getContractorById, customer, contractor, contractorIsLoading} = useContractor()
-  
-  const rutFormated = format(rut)
+  const [rut, setRut] = useState("");
+  const [isActive, setIsActive] = useState(false);
+  const [openTable, setOpenTable] = useState(0);
+  const {
+    getByRutOrName,
+    getContractorById,
+    customer,
+    contractor,
+    contractorIsLoading,
+  } = useContractor();
+
+  const rutFormated = format(rut);
 
   const handleClick = () => {
-    getByRutOrName(rutFormated)
-    setOpenTable(1)
-  }
+    getByRutOrName(rutFormated);
+    setOpenTable(1);
+  };
 
   useEffect(() => {
-    getContractorById(customer.data[0].id)
-  }, [customer])
+    getContractorById(customer.data[0].id);
+  }, [customer]);
 
   useEffect(() => {
     const balances = contractor.origins.map((origin) => origin.balance);
     const hasActiveBalance = balances.some((balance) => balance !== null);
-    setIsActive(hasActiveBalance)
-  }, [contractor])
+    setIsActive(hasActiveBalance);
+  }, [contractor]);
 
   return (
     <ContentCol gap="10px" width="500px">
       <ContentRow>
-        <h1 className='text-xl'>Inserta el rut de una persona para saber su estado</h1>
+        <h1 className="text-xl">
+          Inserta el rut de una persona para saber su estado
+        </h1>
       </ContentRow>
-      <Input onChange={(event: React.ChangeEvent<HTMLInputElement>) => setRut(event.target.value)}/>
-      <Button onClick={handleClick}/>
-      {openTable === 1 && !contractorIsLoading ?
-        <div>
-          {!isActive ? 
-          <Table height="80px">
-            <TableHeader>
-              <TableRow>
-                <TableCell width='400px'>
-                  Nombre
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell width='90px'>
-                  Estado
-                </TableCell>
-              </TableRow>
-              <TableCellEnd></TableCellEnd>
-            </TableHeader>
-            <TableDetail>
-              <TableRow>
-                <TableCell width='400px'>
-                  {customer.data[0].name}
-                </TableCell>
-                <TableCell width='90px'>
-                  Activo
-                </TableCell>
-              </TableRow>
-            </TableDetail>
-          </Table>
-          :
-          <Table height="80px">
-            <TableHeader>
-              <TableRow>
-                <TableCell width='400px'>
-                  Nombre
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell width='90px'>
-                  Estado
-                </TableCell>
-              </TableRow>
-              <TableCellEnd></TableCellEnd>
-            </TableHeader>
-            <TableDetail>
-              <TableRow>
-                <TableCell width='400px'>
-                  {customer.data[0].name}
-                </TableCell>
-                <TableCell width='90px'>
-                  No Activo
-                </TableCell>
-              </TableRow>
-            </TableDetail>
-          </Table>
+      <Input
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          setRut(event.target.value)
         }
+      />
+      <Button onClick={handleClick} />
+      {openTable === 1 && !contractorIsLoading ? (
+        <div>
+          {!isActive ? (
+            <Table height="80px">
+              <TableHeader>
+                <TableRow>
+                  <TableCell width="400px">Nombre</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell width="90px">Estado</TableCell>
+                </TableRow>
+                <TableCellEnd></TableCellEnd>
+              </TableHeader>
+              <TableDetail>
+                <TableRow>
+                  <TableCell width="400px">{customer.data[0].name}</TableCell>
+                  <TableCell width="90px">Activo</TableCell>
+                </TableRow>
+              </TableDetail>
+            </Table>
+          ) : (
+            <Table height="80px">
+              <TableHeader>
+                <TableRow>
+                  <TableCell width="400px">Nombre</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell width="90px">Estado</TableCell>
+                </TableRow>
+                <TableCellEnd></TableCellEnd>
+              </TableHeader>
+              <TableDetail>
+                <TableRow>
+                  <TableCell width="400px">{customer.data[0].name}</TableCell>
+                  <TableCell width="90px">No Activo</TableCell>
+                </TableRow>
+              </TableDetail>
+            </Table>
+          )}
         </div>
-        :
-        null
-      }
+      ) : null}
     </ContentCol>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
---
+++ web-request/src/components/ui/InputText/InputText.tsx
@@ -0,0 +1,74 @@
+import styles from "./Input.module.scss";
+
+type InputT = {
+  id?: string;
+  label?: string;
+  width?: string;
+  type?: string;
+  value?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  icon?: string;
+  display?: boolean;
+  maxLength?: number;
+  isValid?: boolean;
+  disabled?: boolean;
+  className?: string;
+  minDate?: string;
+  minTime?: string;
+  maxTime?: string;
+  step?: string;
+  defaultValue?: string;
+};
+
+const InputText = ({
+  id,
+  label,
+  width,
+  type = "text",
+  value,
+  onChange,
+  icon,
+  display = true,
+  maxLength,
+  isValid = true,
+  disabled = false,
+  className,
+  minDate,
+  minTime,
+  maxTime,
+  step,
+  defaultValue,
+}: InputT) => {
+  let iconName;
+
+  return (
+    <div className={`${styles.inputText} ${className}`} style={{ width }}>
+      <input
+        type={type}
+        autoComplete="none"
+        id={id}
+        value={value}
+        defaultValue={defaultValue}
+        onChange={onChange}
+        placeholder=""
+        className={
+          styles.input +
+          (icon ? " " + styles.withIcon : "") +
+          (isValid ? "" : " " + styles.notValid)
+        }
+        style={{ display: display ? "block" : "none" }}
+        maxLength={maxLength}
+        disabled={disabled}
+        min={minDate ? minDate : minTime}
+        max={maxTime}
+        step={step}
+      />
+      {label && display && <label htmlFor={id}>{label}</label>}
+      {icon && display && (
+        <span className="material-icons md-36">{iconName}</span>
+      )}
+    </div>
+  );
+};
+
+export default InputText;
