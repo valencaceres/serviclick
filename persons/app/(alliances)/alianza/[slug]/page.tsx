905db1bab5033b4cdad1906ec9ace1822aa9e4d0
Claudio Matus Alegría <claudio@matus.cl>
2024-10-04 18:33:08 -0300

    fix/some-fixes

--- persons/app/(alliances)/alianza/[slug]/page.tsx
+++ persons/app/(alliances)/alianza/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next"
 import Image from "next/image"
 import Link from "next/link"
 
-import { config } from "../../../../utils/config";
+import  config from "../../../../utils/config";
 
 import { alliancesData } from "@/lib/alliance-data"
 import { Button } from "@/components/ui/button"
